e84dab58a2a4ef7038a7773c07bc39c970e52477
Гриша <greha1307@gmail.com>
2018-10-31 21:51:30 +0300

    Исправлен код счетчика

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,15 @@
 let animationPlay = false;
 
-let animationAncorElement = document.querySelector('.animation-ancor');
-let pauseBtn = document.querySelector('.pause-btn');
-let muteBtn = document.querySelector('.mute-btn');
+const animationAncorElement = document.querySelector('.animation-ancor');
+const pauseBtn = document.querySelector('.pause-btn');
+const muteBtn = document.querySelector('.mute-btn');
 
-let startDate = new Date(2018, 6);
+const yearBlock = document.querySelector('.year-value');
+const monthBlock = document.querySelector('.month-value');
+const dayBlock = document.querySelector('.day-value');
+const hourBlock = document.querySelector('.hour-value');
+
+const startDate = new Date(2018, 6);
 
 document.querySelector('.activate-element').addEventListener('click', function () {
     muteBtn.innerHTML = "<i class=\"fas fa-volume-down\"></i>";
@@ -39,40 +44,16 @@ muteBtn.addEventListener('click', function () {
 });
 
 function timeTimer() {
-    const yearBlock = document.querySelector('.year-value');
-    const monthBlock = document.querySelector('.month-value');
-    const dayBlock = document.querySelector('.day-value');
-    const hourBlock = document.querySelector('.hour-value');
-
     let timer = setTimeout(function timeCount() {
-        let dif = calcDate(Date.now(), startDate);
+        const now = Date.now();
+        const dif = new Date();
+        dif.setTime(now - startDate);
 
-        yearBlock.innerHTML = dif.years;
-        monthBlock.innerHTML = dif.months;
-        dayBlock.innerHTML = dif.days;
-        hourBlock.innerHTML = dif.hours;
+        yearBlock.innerHTML = (dif.getFullYear() - 1970) + '';
+        monthBlock.innerHTML = dif.getMonth() + '';
+        dayBlock.innerHTML = dif.getDay() + '';
+        hourBlock.innerHTML = dif.getHours() + '';
 
         timer = setTimeout(timeCount, 1000);
     }, 1000);
-}
-
-function calcDate(date1, date2) {
-    let diff = Math.floor(date1 - date2);
-    let day = 1000 * 60 * 60 * 24;
-
-    let hours = Math.floor(diff / (day / 24));
-    let days = Math.floor(diff / day);
-    let months = Math.floor(days / 31);
-    let years = Math.floor(months / 12);
-
-    months -= years * 12;
-    days -= ((years * 12 + months) * 30);
-    hours -= ((((years * 12 + months) * 30) + days) * 24);
-
-    return {
-        years: years,
-        months: months,
-        days: days,
-        hours: hours
-    }
 }
